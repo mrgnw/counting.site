8ce6867aa0e93e52f256339499cbe95e132abee9
Morgan Williams <mwilliams@izeni.com>
2014-11-07 13:59:42 -0700

    sort by votes instead of created date.

--- spacefood.js
+++ spacefood.js
@@ -5,7 +5,7 @@ if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function () {
       // Show newest tasks first
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      return Tasks.find({}, {sort: {votes: -1}});
     }
   });
 
