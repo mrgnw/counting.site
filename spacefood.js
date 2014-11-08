b355da0489e280c072cfa1d758d9ff7e4242a58a
Morgan Williams <m@morganw.me>
2014-11-07 22:41:55 -0700

    securing functions; toggleLike doesnt work yet

--- spacefood.js
+++ spacefood.js
@@ -3,8 +3,7 @@ Snacks = new Mongo.Collection("snacks");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    snacks: function () {
-      // Show most popular snacks first
+    snacks: function () { // Show most popular snacks first
       return Snacks.find({}, {sort: {votes: -1, data_ins: 1}});
     }
 
@@ -15,13 +14,7 @@ if (Meteor.isClient) {
       // This function is called when the new snack form is submitted
       var text = event.target.text.value;
 
-      Snacks.insert({
-        text: text,
-        votes: 1,
-        likers: [Meteor.user().username],
-        createdAt: new Date() // current time
-      });
-
+      Meteor.call("addSnack", text);
       // Clear form
       event.target.text.value = "";
 
@@ -34,18 +27,48 @@ if (Meteor.isClient) {
   Template.snack.events({
   "click .toggle-checked": function () {
     // Set the checked property to the opposite of its current value
-    Snacks.update(this._id, {$set: {checked: ! this.checked}});
+    Meteor.call("setChecked", this._id, ! this.checked);
   },
   "click .delete": function () {
-    Snacks.remove(this._id);
+    Meteor.call("deleteSnack", this._id);
   },
   "click .upVote": function () {
+    Meteor.call("toggleLike", this);
+  }
+});
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+} // isClient
+
+
+Meteor.methods({
+  addSnack: function (text) {
+    // Make sure the user is logged in before inserting a snack
+    if (! Meteor.userId()) { throw new Meteor.Error("not-authorized"); }
+
+    Snacks.insert({
+      text: text,
+      votes: 1,
+      likers: [Meteor.user().username],
+      createdAt: new Date() // current time
+    });
+  },
+  deleteSnack: function (SnackId) {
+    Snacks.remove(SnackId);
+  },
+  setChecked: function (SnackId, setChecked) {
+    Snacks.update(SnackId, { $set: { checked: setChecked} });
+  },
+  toggleLike: function(snack) {
     // If logged in:
     var user = Meteor.user().username;
     likers = Snacks.findOne(this._id).likers;
     isLiker = (likers.indexOf(user) != -1);
 
-    var dislike = function(snack) { // -1, remove user from likers
+    function dislike (snack) { // -1, remove user from likers
       Snacks.update(snack._id, {$set: {votes: snack.votes - 1 }});
       Snacks.update(snack._id, {$pull: {likers: user }});
     }
@@ -65,10 +88,5 @@ if (Meteor.isClient) {
       // todo: ask to log in.
     }
   }
+
 });
-
-  Accounts.ui.config({
-    passwordSignupFields: "USERNAME_ONLY"
-  });
-
-} // isClient
