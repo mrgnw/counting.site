2d507e509cf4a467d8bc48c9d4c0f62712b3ce22
Morgan Williams <m@morganw.me>
2014-11-14 13:56:24 -0700

    added mobile config

---
+++ mobile-config.js
@@ -0,0 +1,15 @@
+App.info({
+  name: 'Punch.it',
+  description: 'Simple daily goal tracking',
+  version: '0.0.1'
+});
+
+// App.icons({
+//
+// })
+
+// App.launchScreens({
+//   'iphone': '...',
+//   'iphone_2x': '...',
+//   'iphone5': '...'
+// });
